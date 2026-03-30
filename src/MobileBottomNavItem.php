8c9ab762704949a8c46241a47bfc064b71af9453
Ching Cheng Kang <pycck@hotmail.com>
2026-03-30 11:46:10 +0800

    refactor: add label() setter instead of Closure in make()
    Add a label(string|Closure) setter method for deferred label resolution,
    keeping make(string) as a simple identifier. This follows Filament's
    convention where make() sets the ID and label() sets the display text.

--- src/MobileBottomNavItem.php
+++ src/MobileBottomNavItem.php
@@ -27,11 +27,20 @@ class MobileBottomNavItem
 
     protected bool | Closure $isVisible = true;
 
-    final public function __construct(protected string | Closure $label) {}
+    protected string | Closure | null $labelOverride = null;
 
-    public static function make(string | Closure $label): static
+    final public function __construct(protected string $label) {}
+
+    public static function make(string $label): static
     {
         return new static($label);
+    }
+
+    public function label(string | Closure $label): static
+    {
+        $this->labelOverride = $label;
+
+        return $this;
     }
 
     public function icon(string | BackedEnum | Htmlable $icon): static
@@ -96,8 +105,12 @@ class MobileBottomNavItem
 
     public function getLabel(): string
     {
-        if ($this->label instanceof Closure) {
-            return ($this->label)();
+        if ($this->labelOverride instanceof Closure) {
+            return ($this->labelOverride)();
+        }
+
+        if ($this->labelOverride !== null) {
+            return $this->labelOverride;
         }
 
         return $this->label;
